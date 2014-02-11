62a2d011040dbb8696c2c983fa199d4645b012ae
mhippocrate@gmail.com <mickaelh@users.noreply.github.com>
2014-02-11 10:49:09 -0400

    Update text

--- text2wav.py
+++ text2wav.py
@@ -24,7 +24,7 @@ How to use this script:
     - selected your text and copy (ctrl + c) and executed a command terminal
     $ ./text2wav.py
 
-In the directory will generate the article1.wav file article2.wav ...
+In the current directory of text2wav.py it will generate the article1.wav file article2.wav ...
 good listening.
 
 TODO:
